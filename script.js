50a0b0a14fcd0e8a73fbf617278b590bb673eb4c
ZarrukhJurakulov <zarrukhjurakulov474@gmail.com>
2021-04-24 15:42:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function starterButton() {
+ var gettingText = document.getElementById("starterInput").value;
+ document.getElementById("demo").innerHTML = gettingText
+}
+
+
+
+addTwoNums(2, 3)
+//5
+
+addTwoNums(5, 3)
+//8
+function myFunction(a, b) {
+ return a + b;
+}
